58d593736a3f7f595984cf66896143d222f80057
on1page <coccodicarlo245@gmail.com>
2026-03-03 18:32:15 +0100

    Update db.ts for PostgreSQL

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,11 +8,6 @@ export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['query'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
-      }
-    }
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
